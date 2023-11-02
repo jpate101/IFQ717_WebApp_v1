05e333d2a88bedbbeb857b0cd6a960be805498ca
LizzyG <lizzy.giglio@gmail.com>
2023-11-02 20:14:40 +1000

    Updated css

--- ifq717-project-v1/tailwind.config.js
+++ ifq717-project-v1/tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js, jsx}"],
+  content: [
+    "./src/**/*.{js, jsx, css}",
+    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js"
+  ],
   theme: {
     extend: {
       tandaBlue: '#19b3d9',
