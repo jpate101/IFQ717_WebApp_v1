d7bf7ef902ebdb4dd875784efce0de20e76c1c55
LizzyG <lizzy.giglio@gmail.com>
2023-11-02 19:59:26 +1000

    Added Scheduler

--- ifq717-project-v1/tailwind.config.js
+++ ifq717-project-v1/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js, jsx}"],
   theme: {
     extend: {
+      tandaBlue: '#19b3d9',
       colors: {
         'primary': '#3498db',  // blue-500
         'secondary': '#1e40af',  // blue-800
