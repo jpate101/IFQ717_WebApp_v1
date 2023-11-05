27de343490526331fc2548fad0dab9face4ca579
LizzyG <lizzy.giglio@gmail.com>
2023-11-05 20:57:16 +1000

    modal components format function

--- ifq717-project-v1/src/Components/Roster&Timesheets/FormItemWrapper.js
+++ ifq717-project-v1/src/Components/Roster&Timesheets/FormItemWrapper.js
@@ -3,7 +3,7 @@ import React from 'react';
 function FormItemWrapper({ children, icon}) {
     return (
       <div className="flex">
-        {icon && <div className="text-tandaBlue mr-2">{icon}</div>}
+        {icon && <div className="text-tandaBlue mr-2 mt-1">{icon}</div>}
         {children}
       </div>
     );
