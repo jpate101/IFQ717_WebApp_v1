97f015ab5452c21a377261456da17190a261bb10
LizzyG <lizzy.giglio@gmail.com>
2023-11-15 16:10:21 +1000

    updated user's timesheet authorisation

--- ifq717-project-v1/src/App.js
+++ ifq717-project-v1/src/App.js
@@ -70,7 +70,7 @@ export default function App() {
             <Route
               path="Timesheets/:userId"
               element={<PrivateRoute element={<TimesheetForUser />}
-              isLoggedIn={isLoggedIn} />}
+              authorised={isLoggedIn} />}
             />
             <Route
               path="/Timesheets/exportTimesheets"
