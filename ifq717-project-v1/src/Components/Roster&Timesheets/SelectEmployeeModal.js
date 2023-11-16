a5ebdfd4125bf06bfcb23bc4cb79b4508c583085
LizzyG <lizzy.giglio@gmail.com>
2023-11-16 14:18:19 +1000

    Created a select employee modal

---
+++ ifq717-project-v1/src/Components/Roster&Timesheets/SelectEmployeeModal.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Modal, Select, Button } from 'antd';
+import FormItemWrapper from './FormItemWrapper';
+import { ReactComponent as PersonPlusIcon } from '../../svg/person-plus.svg'
+
+const SelectEmployeeModal = ({ isOpen, onClose, employees, onEmployeeSelect }) => {
+  const handleEmployeeChange = value => {
+    onEmployeeSelect(value);
+  };
+
+  const handleSave = () => {
+    onClose();
+  };
+
+  const modalFooter = (
+    <div>
+      <Button 
+        onClick={handleSave}
+        type="primary"
+        style={{
+            color: 'white',
+            borderColor: '#3498db',
+            backgroundColor: '#3498db'
+        }}>
+        Save
+      </Button>
+    </div>
+  );
+
+  return (
+    <Modal
+      title="Select Employee"
+      visible={isOpen}
+      onCancel={onClose}
+      onOk={handleSave}
+      footer={modalFooter}
+      closeIcon
+      className="fixed inset-0 flex justify-center items-center"
+    >
+        <FormItemWrapper>
+            <PersonPlusIcon
+            className="tanda-icon"
+            >
+            </PersonPlusIcon>
+            <Select
+                showSearch
+                style={{ width: '100%' }}
+                placeholder="Select an employee"
+                onChange={handleEmployeeChange}
+            >
+                {employees.map(employee => (
+                <Select.Option key={employee.id} value={employee.id}>
+                    {employee.name}
+                </Select.Option>
+                ))}
+            </Select>
+        </FormItemWrapper>
+    </Modal>
+  );
+};
+
+export default SelectEmployeeModal;
+
