2728bb58d816c477e653582d8346b8d8403c1b9a
LizzyG <lizzy.giglio@gmail.com>
2023-11-18 19:14:44 +1000

    Updated leave sidebar, photo now works

--- ifq717-project-v1/src/Components/Leave/LeaveSidebar.js
+++ ifq717-project-v1/src/Components/Leave/LeaveSidebar.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { Offcanvas, Form, Button, FormCheck } from 'react-bootstrap';
-import { DatePicker, Space, Input } from 'antd';
+import { Offcanvas, Dropdown, DropdownButton, Form, Button, FormCheck } from 'react-bootstrap';
+import { DatePicker, Space, Input, ConfigProvider } from 'antd';
 import { getCurrentUser, getLeaveTypesForUser, createLeaveRequest } from '../../API/Utilities';
 import dayjs from 'dayjs';
+
 const { RangePicker } = DatePicker;
+
+const BASE_URL = 'https://my.tanda.co';
 
 const LeaveSidebar = ({ show, handleClose }) => {
     const [currentUser, setCurrentUser] = useState({});
@@ -25,8 +28,9 @@ const LeaveSidebar = ({ show, handleClose }) => {
         try {
             const userData = await getCurrentUser();
             console.log('Fetched user data:', userData);
+            console.log('User photo URL:', userData.photo);
             setCurrentUser(userData);
-            setLeaveRequest((lr) => ({ ...lr, user_id: userData.id })); // Set user_id here
+            setLeaveRequest((lr) => ({ ...lr, user_id: userData.id }));
             const userLeaveTypes = await getLeaveTypesForUser(userData.id);
             console.log('Fetched leave types:', userLeaveTypes);
             setLeaveTypes(userLeaveTypes);
@@ -40,14 +44,23 @@ const LeaveSidebar = ({ show, handleClose }) => {
     const handleDateTimeChange = (_, formattedDates) => {
         console.log('DateTime Change - Selected Time: ', formattedDates);
         if (formattedDates.length === 2 && formattedDates[0] && formattedDates[1]) {
-            const startDate = dayjs(formattedDates[0], 'YYYY-MM-DD HH:mm');
-            const endDate = dayjs(formattedDates[1], 'YYYY-MM-DD HH:mm');
+            const formatString = isAllDay ? 'YYYY-MM-DD' : 'YYYY-MM-DD HH:mm';
+            const startDate = dayjs(formattedDates[0], formatString);
+            const endDate = dayjs(formattedDates[1], formatString);
+    
+            if (!startDate.isValid() || !endDate.isValid()) {
+                console.error("Invalid date format");
+                return;
+            }
+    
+            console.log('Parsed Start Date:', startDate.format());
+            console.log('Parsed End Date:', endDate.format()); 
             const updatedRequest = {
                 ...leaveRequest,
                 start: startDate.format('YYYY-MM-DD'),
-                start_time: '2000-01-01 ' + startDate.format('HH:mm:ss'),
+                start_time: isAllDay ? '2000-01-01 00:00:00' : startDate.format('YYYY-MM-DD HH:mm:ss'),
                 finish: endDate.format('YYYY-MM-DD'),
-                finish_time: '2000-01-01 ' + endDate.format('HH:mm:ss'),
+                finish_time: isAllDay ? '2000-01-01 23:59:59' : endDate.format('YYYY-MM-DD HH:mm:ss'),
             };
             console.log('Updated leave request after date change:', updatedRequest);
             setLeaveRequest(updatedRequest);
@@ -55,12 +68,13 @@ const LeaveSidebar = ({ show, handleClose }) => {
             console.error("Invalid date selection");
         }
     };
+    
 
     const handleCreateLeaveRequest = async () => {
         console.log('Creating Leave Request with:', leaveRequest);
         const requestData = {
             ...leaveRequest,
-            allDay: isAllDay,
+            all_day: isAllDay,
             user_id: currentUser.id,
             status: 'pending',
         };
@@ -72,7 +86,7 @@ const LeaveSidebar = ({ show, handleClose }) => {
             alert('Leave Request Successfully Submited');
         } catch (error) {
             console.error('Error creating leave request:', error);
-            console.error('Error details:', error.response || error); // Log detailed error information
+            console.error('Error details:', error.response || error);
         }
     };
     
@@ -84,40 +98,66 @@ const LeaveSidebar = ({ show, handleClose }) => {
         </Offcanvas.Header>
         <Offcanvas.Body>
             <div>
-            <img src={currentUser.photo} alt={currentUser.name} style={{ width: '100px', height: '100px' }} />
-            <h3>{currentUser.name}</h3>
+            <img 
+                src={`${BASE_URL}${currentUser.photo}`}
+                alt={currentUser.name} 
+                style={{ width: '100px', height: '100px' }}
+            />
+            <h3 className="mt-3">{currentUser.name}</h3>
             </div>
             <Form>
-            <Form.Group>
-                <Form.Label>Leave Type</Form.Label>
-                <Form.Control as="select" onChange={(e) => setLeaveRequest({...leaveRequest, leave_type: e.target.value})}>
-                {leaveTypes.map((type, index) => (
-                    <option key={index} value={type}>{type}</option>
-                ))}
-                </Form.Control>
-            </Form.Group>
-            <Form.Group>
-                <Form.Label>All-day</Form.Label>
-                <FormCheck
-                    type="checkbox"
-                    role="switch"
-                    checked={isAllDay}
-                    onChange={() => setIsAllDay(!isAllDay)}
-                />
-            </Form.Group>
-            <Space direction ="vertical" size={12}>
-                <RangePicker
-                    showTime={!isAllDay}
-                    format={isAllDay ? "YYYY-MM-DD" : "YYYY-MM-DD HH:mm"}
-                    onChange={handleDateTimeChange}
-                    minuteStep={15}
-                />
-            </Space>
-            <Form.Group>
-                <Form.Label>Reason</Form.Label>
-                <Input.TextArea rows={4} onChange={(e) => setLeaveRequest({...leaveRequest, reason: e.target.value})} />
-            </Form.Group>
-            <Button variant="primary" onClick={handleCreateLeaveRequest}>Create</Button>
+                <Form.Group>
+                    <DropdownButton  
+                        title={leaveRequest.leave_type || "Select Leave Type"} 
+                        onSelect={(eventKey) => setLeaveRequest({...leaveRequest, leave_type: eventKey})}
+                        className="leave-custom-dropdown"
+                    >
+                        {leaveTypes.map((type, index) => (
+                            <Dropdown.Item 
+                                key={index} 
+                                eventKey={type}
+                            >
+                                {type}
+                            </Dropdown.Item>
+                        ))}
+                    </DropdownButton>
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>All-day</Form.Label>
+                    <FormCheck
+                        type="checkbox"
+                        role="switch"
+                        checked={isAllDay}
+                        onChange={() => setIsAllDay(!isAllDay)}
+                        className="mb-3"
+                    />
+                </Form.Group>
+                <Space direction ="vertical" size={12}>
+                    <ConfigProvider>
+                        <RangePicker
+                            showTime={!isAllDay}
+                            format={isAllDay ? "YYYY-MM-DD" : "YYYY-MM-DD HH:mm"}
+                            onChange={handleDateTimeChange}
+                            minuteStep={15}
+                            className="mb-3"
+                        />
+                    </ConfigProvider>
+                </Space>
+                <Form.Group>
+                    <Form.Label>Reason</Form.Label>
+                    <Input.TextArea
+                        rows={4}
+                        onChange={(e) => setLeaveRequest({...leaveRequest, reason: e.target.value})} 
+                        
+                    />
+                </Form.Group>
+                <Button
+                    variant="primary"
+                    onClick={handleCreateLeaveRequest}
+                    className="create-button"
+                >
+                    Create
+                </Button>
             </Form>
         </Offcanvas.Body>
         </Offcanvas>
