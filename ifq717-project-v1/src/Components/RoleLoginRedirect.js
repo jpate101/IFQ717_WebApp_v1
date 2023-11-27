734dfeaa117d6627f4cc4270fd0a53ef7c3af76f
LizzyG <lizzy.giglio@gmail.com>
2023-11-27 20:14:53 +1000

    Fixed redirect bug

--- ifq717-project-v1/src/Components/RoleLoginRedirect.js
+++ ifq717-project-v1/src/Components/RoleLoginRedirect.js
@@ -1,18 +1,20 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const RoleLoginRedirect = ({ isLoggedIn, userRole }) => {
   const navigate = useNavigate();
+  const [hasRedirected, setHasRedirected] = useState(false);
 
   useEffect(() => {
-    if (isLoggedIn && userRole) {
+    if (isLoggedIn && userRole && !hasRedirected) {
       if (userRole === 'manager') {
         navigate('/dashboard');
       } else if (userRole === 'employee') {
         navigate('/EmployeeDashboard');
       }
+      setHasRedirected(true);
     }
-  }, [isLoggedIn, userRole, navigate]);
+  }, [isLoggedIn, userRole, navigate, hasRedirected]);
 
   return null;
 };
