a639dfb3d49aa1224b4f3f244cf9c3ea81528736
LizzyG <lizzy.giglio@gmail.com>
2023-12-03 08:29:38 +1000

    token now returns a value indicating login success

--- ifq717-project-v1/src/Hooks/useTokenAuthentication.js
+++ ifq717-project-v1/src/Hooks/useTokenAuthentication.js
@@ -11,51 +11,50 @@ export default function useTokenAuthentication({ username, password }) {
   const scopes = 'me user department cost financial roster timesheet leave unavailability qualifications settings sms platform';
   const grantType = 'password';
 
-  const handleTokenExchange = () => {
-    fetch(tokenUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Cache-Control': 'no-cache'
-      },
-      body: new URLSearchParams({
-        username: username,
-        password: password,
-        scope: scopes,
-        grant_type: grantType
-      })
-    }).then((res) => {
-        if (res.ok) {
-          return res.json();
-        } else if (res.status === 402) {
+  const handleTokenExchange = async () => {
+    try {
+      const res = await fetch(tokenUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Cache-Control': 'no-cache'
+        },
+        body: new URLSearchParams({
+          username: username,
+          password: password,
+          scope: scopes,
+          grant_type: grantType
+        })
+      });
+
+      const data = await res.json();
+      console.log('Token exchange response: ', data);
+
+      if (res.ok) {
+        // Store bearer token to cookie
+        document.cookie = `token=${data.access_token}; path=/;`;
+        // set login state to true on login success!
+        setIsLoggedIn(true);
+        // redirect to dashboard on successful login
+        navigate('/dashboard');
+        return true;
+      } else {
+        // display error on login failure
+        if (res.status === 402) {
           throw new Error('Your account is locked for billing purposes. Please contact your Tanda Manager for support.');
         } else if (res.status === 409) {
           throw new Error('Your account is rate limited. Please try again in 1 minute.');
+        } else {
+          throw new Error(data.error_description || 'Login failed');
         }
-      }).then((data) => {
-      console.log('Token exchange response: ', data);
-      if (data.error) {
-        console.error('Token exchange error: ', data.error);
-
-        // display error on login failure 
-        setMessage(data.error_description);
-
-        // set login state to false on login failure
-        setIsLoggedIn(false);
-      } else {
-        // Store bearer token to cookie
-        document.cookie = `token=${data.access_token}; path=/;`;
-
-        // set login state to true on login success!
-        setIsLoggedIn(true);
-
-        // redirect to dashboard on successful login
-        navigate('/dashboard');
       }
-    }).catch((error) => {
+    } catch (error) {
       console.error('Error on swapping auth code for token: ', error);
-      setMessage('An error occurred on Login. Please check credentials and try again.');
-    });
+      setMessage(error.message || 'An error occurred on Login. Please check credentials and try again.');
+      // set login state to false on login failure
+      setIsLoggedIn(false);
+      return false;
+    }
   };
 
   return { message, isLoggedIn, handleTokenExchange };
