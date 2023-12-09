6ff27ddc05265a5192fcb9b86620284142e75997
holly Marshall <holler4amarshall@hotmail.com>
2023-12-09 16:40:43 +1100

    clockins: set invited & mobile use status

--- ifq717-project-v1/src/ClockIn/ClockInUserGrid.js
+++ ifq717-project-v1/src/ClockIn/ClockInUserGrid.js
@@ -1,5 +1,7 @@
+// getting a list of active users only & building table to display if user invited, active, clocked in etc. 
+
 import React, { useState, useEffect } from 'react';
-import { AgGridReact, AgGridColumn } from 'ag-grid-react';
+import { AgGridReact } from 'ag-grid-react';
 import '../../node_modules/ag-grid-community/styles/ag-grid.css';
 import '../../node_modules/ag-grid-community/styles/ag-theme-alpine.css'; 
 import { getUsers } from '../API/Utilities';
@@ -17,20 +19,31 @@ function ClockInUserGrid() {
   useEffect(() => {
     const fetchData = async () => {
       const users = await getUsers();
-      setRowData(users);
+      console.log('Server response:', users); // TODO: remember to remove! 
+  
+      const activeUsers = users.filter(user => user.active).map(user => {
+        const isInvited = user.last_synced_mobile_app !== null;
+        const isAppActive = user.last_synced_mobile_app !== 0;
+        const invited = isInvited ? "Invited" : "Invite";
+        const mobileApp = isAppActive ? (user.last_synced_mobile_app !== null ? user.last_synced_mobile_app.toString() : null) : (user.last_synced_mobile_app === 0 ? "Never" : null);
+  
+        console.log('My calculated data:', { ...user, isInvited, isAppActive, invited, mobileApp }); // TODO: remember to remove! 
+  
+        return { ...user, isInvited, isAppActive, invited, mobileApp };
+      });
+  
+      setRowData(activeUsers);
     };
-
+  
     fetchData();
   }, []);
 
   const columns = [
     { headerName: "Name", field: "name", sortable: true, filter: true },
-    { headerName: "Active", field: "active", sortable: true, filter: true },
     { headerName: "Start date", field: "employment_start_date", sortable: true, filter: false },
-    //{ headerName: "1st / Next Shift", field: "firstShift", sortable: true, filter: true },
-    { headerName: "Clockin Invited", field: "invite", sortable: true, filter: true },
+    { headerName: "Invited", field: "invited", sortable: true, filter: true },
+    { headerName: "MobileApp", field: "mobileApp", sortable: true, filter: true },
     { headerName: "1st Clockin", field: "first_clockin", sortable: true, filter: true },
-    { headerName: "Reminder", field: "reminder", sortable: true, filter: true }
   ];
 
   return (
